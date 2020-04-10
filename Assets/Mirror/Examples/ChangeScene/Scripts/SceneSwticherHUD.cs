15c2f351a62f7ffc8055535d256fac25eb6ff982
uwee <uweenukr@gmail.com>
2020-04-09 23:41:19 -0400

    add and remove scene buttons

--- Assets/Mirror/Examples/ChangeScene/Scripts/SceneSwticherHUD.cs
+++ Assets/Mirror/Examples/ChangeScene/Scripts/SceneSwticherHUD.cs
@@ -1,13 +1,16 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwticherHUD : MonoBehaviour
 {
     public NetworkManager networkManager;
 
+    string sceneText;
+
     void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(10, 400, 215, 9999));
+        GUILayout.BeginArea(new Rect(10, 200, 215, 9999));
 
         if (GUILayout.Button("Switch to Room1"))
         {
@@ -19,6 +22,40 @@ public class SceneSwticherHUD : MonoBehaviour
             networkManager.ServerChangeScene("Room2");
         }
 
+        if (GUILayout.Button("Add Room1 Scene"))
+        {
+            SceneManager.LoadSceneAsync("Room1", new LoadSceneParameters()
+            {
+                loadSceneMode = LoadSceneMode.Additive,
+                localPhysicsMode = LocalPhysicsMode.Physics3D
+            });
+        }
+
+        if (GUILayout.Button("Remove All Scenes"))
+        {
+            RemoveScenes();
+        }
+
+        GUILayout.Label("List of Active Scenes:" + "\n" + sceneText);
+
         GUILayout.EndArea();
+    }
+
+    public void FixedUpdate()
+    {
+        int countLoaded = SceneManager.sceneCount;
+        Scene[] loadedScenes = new Scene[countLoaded];
+ 
+        sceneText = string.Empty;
+
+        for (int i = 0; i < countLoaded; i++)
+        {
+            sceneText += SceneManager.GetSceneAt(i).name + "\n";
+        }
+    }
+
+    private void RemoveScenes()
+    {
+        
     }
 }
